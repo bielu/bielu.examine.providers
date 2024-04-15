a33379fe0c75a020d4cd049bc47b0308c8dcc489
mcl-sz <sjouke.zonneveld@znb.nl>
2024-04-15 18:06:30 +0200

    Analyzer and TransformingIndexValues

--- src/Bielu.Examine.ElasticSearch/Services/IndexStateService.cs
+++ src/Bielu.Examine.ElasticSearch/Services/IndexStateService.cs
@@ -1,4 +1,4 @@
-﻿using Bielu.Examine.Core.Models;
+using Bielu.Examine.Core.Models;
 using Bielu.Examine.Core.Services;
 using Bielu.Examine.Elasticsearch.Configuration;
 using Bielu.Examine.Elasticsearch.Model;
@@ -8,20 +8,21 @@ namespace Bielu.Examine.Elasticsearch.Services;
 
 public class IndexStateService(IOptionsMonitor<BieluExamineElasticOptions> examineElasticOptions) : IIndexStateService
 {
-    private Dictionary<string,ExamineIndexState> _indexStates = new Dictionary<string,ExamineIndexState>();
+    private Dictionary<string, ExamineIndexState> _indexStates = [];
     public ExamineIndexState GetIndexState(string indexName)
     {
-        if(_indexStates.TryGetValue(indexName, out var state))
+        if (_indexStates.TryGetValue(indexName, out var state))
         {
             return state;
         }
         var elasticConfig = examineElasticOptions.CurrentValue;
         var configuration = elasticConfig.IndexConfigurations.FirstOrDefault(x => x.Name.Equals(indexName, StringComparison.OrdinalIgnoreCase));
-        state = new ExamineIndexState();
-        state.IndexName = indexName;
-
-        state.Analyzer = configuration?.Analyzer;
-        var prefix=(configuration?.Prefix?.ToLowerInvariant() ?? elasticConfig.DefaultIndexConfiguration?.Prefix)?.ToLowerInvariant();
+        state = new ExamineIndexState
+        {
+            IndexName = indexName,
+            Analyzer = configuration?.Analyzer ?? elasticConfig.DefaultIndexConfiguration?.Analyzer
+        };
+        var prefix = (configuration?.Prefix?.ToLowerInvariant() ?? elasticConfig.DefaultIndexConfiguration?.Prefix)?.ToLowerInvariant();
         if (!string.IsNullOrWhiteSpace(prefix))
         {
             prefix += "_";
