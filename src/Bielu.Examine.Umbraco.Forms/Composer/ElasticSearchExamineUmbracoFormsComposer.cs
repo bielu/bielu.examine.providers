b13d49330d02e78164efa6df9216bc3b99376caf
mcl-sz <sjouke.zonneveld@znb.nl>
2024-07-05 15:59:28 +0200

    Empty Forms FieldDefinitions to prevent adding default UmbracoFieldDefinitions

--- src/Bielu.Examine.Umbraco.Forms/Composer/ElasticSearchExamineUmbracoFormsComposer.cs
+++ src/Bielu.Examine.Umbraco.Forms/Composer/ElasticSearchExamineUmbracoFormsComposer.cs
@@ -1,16 +1,18 @@
-﻿using Bielu.Examine.Core.Configuration;
+using Bielu.Examine.Core.Configuration;
 using Bielu.Examine.Core.Services;
 using Bielu.Examine.ElasticSearch.Umbraco.Form.Indexer;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using bielu.Examine.Umbraco.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Bielu.Examine.ElasticSearch.Umbraco.Form.Configuration;
 namespace Bielu.Examine.ElasticSearch.Umbraco.Form.Composer;
 public static class BieluExamineConfiguratorExtensions
 {
 
     public static BieluExamineConfigurator AddFormProvider(this BieluExamineConfigurator builder)
     {
-        builder.ServiceCollection.AddBieluExamineIndex<BieluExamineUmbracoFormsIndex>(global::Umbraco.Forms.Core.Constants.ExamineIndex.RecordIndexName);
+        builder.ServiceCollection.AddBieluExamineIndex<BieluExamineUmbracoFormsIndex>(global::Umbraco.Forms.Core.Constants.ExamineIndex.RecordIndexName).ConfigureOptions<ConfigureUmbracoFormsIndexOptions>();
         return builder;
     }
 }
