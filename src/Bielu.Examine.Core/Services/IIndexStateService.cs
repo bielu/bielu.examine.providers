388b65b0ee747531145b940e0070511bd3653763
mcl-sz <sjouke.zonneveld@znb.nl>
2024-07-07 15:41:58 +0200

    Adding ISearchService to GetIndexState to check if the index exists

--- src/Bielu.Examine.Core/Services/IIndexStateService.cs
+++ src/Bielu.Examine.Core/Services/IIndexStateService.cs
@@ -1,8 +1,8 @@
-﻿using Bielu.Examine.Core.Models;
+using Bielu.Examine.Core.Models;
 
 namespace Bielu.Examine.Core.Services;
 
 public interface IIndexStateService
 {
-    public ExamineIndexState GetIndexState(string indexName);
+    public ExamineIndexState GetIndexState(string indexName, ISearchService searchService);
 }
